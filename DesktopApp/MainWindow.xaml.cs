e5bbfccdacd3f534f0645719d36c76a69ec6dc21
p-majchrzak <148012675+p-majchrzak@users.noreply.github.com>
2024-10-02 18:59:44 +0200

    Add project files.

---
+++ DesktopApp/MainWindow.xaml.cs
@@ -0,0 +1,91 @@
+﻿using ConsoleApp;
+using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DesktopApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Music> musicList = new List<Music>();
+        public int currentMusicId = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+            musicList = GetMusicList(Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\Data.txt");
+            RefreshMusic(currentMusicId);
+        }
+        public static List<Music> GetMusicList(string path)
+        {
+            List<Music> musicList = new List<Music>();
+            StreamReader readText = new StreamReader(path);
+
+            while (!readText.EndOfStream)
+            {
+                string artist = readText.ReadLine();
+                string album = readText.ReadLine();
+                int songsNumber = int.Parse(readText.ReadLine());
+                int year = int.Parse(readText.ReadLine());
+                int downloadNumber = int.Parse(readText.ReadLine());
+
+                string skipRecord = readText.ReadLine();
+
+                Music music = new Music(artist, album, songsNumber, year, downloadNumber);
+                musicList.Add(music);
+            }
+
+            readText.Close();
+
+            return musicList;
+        }
+
+        public void RefreshMusic(int id)
+        {
+            if(id < 0)
+            {
+                id = musicList.Count-1;
+                currentMusicId = id;
+            }
+
+            if(id == musicList.Count)
+            {
+                id = 0;
+                currentMusicId = id;
+            }
+
+            Artist.Content = musicList[id].artist;
+            Album.Content = musicList[id].album;
+            SongsNumber.Content = musicList[id].songsNumber + " utworów";
+            Year.Content = musicList[id].year;
+            DownloadNumber.Content = musicList[id].downloadNumber;
+        }
+        private void DownloadBTN_Click(object sender, RoutedEventArgs e)
+        {
+            Music music = musicList[currentMusicId];
+            music.downloadNumber += 1;
+            musicList[currentMusicId] = music;
+            RefreshMusic(currentMusicId);
+        }
+
+        private void PreviousMusicBTN_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshMusic(--currentMusicId);
+        }
+
+        private void NextMusicBTN_Click(object sender, RoutedEventArgs e)
+        {
+            RefreshMusic(++currentMusicId);
+        }
+    }
+}
---
+++ DesktopApp/Music.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    public struct Music
+    {
+        public string? artist { get; set; }
+        public string? album { get; set; }
+        public int songsNumber { get; set; }
+        public int year { get; set; }
+        public int downloadNumber { get; set; }
+        public Music(string artist, string album, int songsNumber, int year, int downloadNumber)
+        {
+            this.album = album;
+            this.artist = artist;
+            this.songsNumber = songsNumber;
+            this.year = year;
+            this.downloadNumber = downloadNumber;
+        }
+    }
+}
